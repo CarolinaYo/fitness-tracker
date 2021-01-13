fda1b4b633d263f18117de6e939f4f44b3e6f2a9
CarolY <mon_alisa75@yahoo.com>
2021-01-13 11:03:58 -0800

    Updated Workout Schema and import.

---
+++ models/Workout.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    
+    day: {
+        type: Date,
+        default: Date.now,
+    },
+    exercises: [
+          {
+            type: {
+                type: String,
+                unique: false,
+            },
+
+            name: {
+                type: String,
+                unique: false
+            },
+
+            duration: {
+                type: Number,
+                unique: false
+            },
+            
+            distance: {
+                type: Number,
+                unique: false
+            },
+
+            weight: {
+                type: Number,
+                unique: false            },
+
+            reps: {
+                type: Number,
+                unique: false            },
+
+            sets: {
+                type: Number,
+                unique: false            }
+
+          }
+        ]
+        
+//   name: {
+//     type: String,
+//     unique: true
+//   },
+//   notes: [
+//     {
+//       type: Schema.Types.ObjectId,
+//       ref: "Note"
+//     }
+//   ]
+});
+
+const User = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = User;
