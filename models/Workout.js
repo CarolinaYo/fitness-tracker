907d0bff8b85654a73aa8cd07ac342b56e0589ca
CarolY <mon_alisa75@yahoo.com>
2021-01-13 11:39:45 -0800

    Updated file name.

--- models/Workout.js
+++ models/Workout.js
@@ -3,58 +3,65 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    
-    day: {
-        type: Date,
-        default: Date.now,
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        unique: false,
+      },
+
+      name: {
+        type: String,
+        unique: false,
+      },
+
+      duration: [
+        {
+          type: Number,
+          unique: false,
+        },
+      ],
+
+      distance: [
+        {
+          type: Number,
+          unique: false,
+        },
+      ],
+
+      weight: [
+        {
+          type: Number,
+          unique: false,
+        },
+      ],
+
+      reps: {
+        type: Number,
+        unique: false,
+      },
+
+      sets: {
+        type: Number,
+        unique: false,
+      },
     },
-    exercises: [
-          {
-            type: {
-                type: String,
-                unique: false,
-            },
+  ],
 
-            name: {
-                type: String,
-                unique: false
-            },
-
-            duration: {
-                type: Number,
-                unique: false
-            },
-            
-            distance: {
-                type: Number,
-                unique: false
-            },
-
-            weight: {
-                type: Number,
-                unique: false            },
-
-            reps: {
-                type: Number,
-                unique: false            },
-
-            sets: {
-                type: Number,
-                unique: false            }
-
-          }
-        ]
-        
-//   name: {
-//     type: String,
-//     unique: true
-//   },
-//   notes: [
-//     {
-//       type: Schema.Types.ObjectId,
-//       ref: "Note"
-//     }
-//   ]
+  //   name: {
+  //     type: String,
+  //     unique: true
+  //   },
+  //   notes: [
+  //     {
+  //       type: Schema.Types.ObjectId,
+  //       ref: "Note"
+  //     }
+  //   ]
 });
 
 const User = mongoose.model("Workout", WorkoutSchema);
