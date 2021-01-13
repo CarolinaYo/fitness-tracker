6e2eec401429cd40ea48fae04e03374b857985fa
CarolY <mon_alisa75@yahoo.com>
2021-01-13 13:36:33 -0800

    set localhost/workout.

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
-
 const app = express();
 
 app.use(logger("dev"));
@@ -18,8 +16,11 @@ app.use(express.static("public"));
 // require("./routes/apiRoutes.js")(app);
 require("./routes/htmlRoutes.js")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
-
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
